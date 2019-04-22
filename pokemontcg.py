b80e5d7105dc7d621724c7d5bceef10132095e1f
Aaron Yam <aaronyam@HON-2016039.local>
2019-04-22 16:05:40 -0400

    Able to receive json back from api call, exit code 200

--- pokemontcg.py
+++ pokemontcg.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 from pokemontcgsdk import Card
 from pokemontcgsdk import Set
 from pokemontcgsdk import Type
 from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
-#from pokemontcgsdk import requests
+import requests
+import json
 
 #create variable to get user input
 usr_input = input('Search Pokemon Name ')
@@ -11,5 +14,5 @@ usr_input = input('Search Pokemon Name ')
 #api request through this link https://api.pokemontcg.io/v1/cards?name=usr_input
 
 #create request object for API call
-#req = requests.get("https://api.pokemontcg.io/v1/cards?name=%s" % usr_input)
-#req.json()
+req = requests.get("https://api.pokemontcg.io/v1/cards?name=%s" %usr_input)
+print(req.status_code)
