b4a56072502c597bab322afe06dc63b3642a3c56
Aaron Yam <aaronyam@HON-2016039.local>
2019-04-22 11:44:39 -0400

    Initial commit of SDK library

---
+++ tests/test_card.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of pokemontcgsdk.
+# https://github.com/PokemonTCG/pokemon-tcg-sdk-python
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2016, Andrew Backes <backes.andrew@gmail.com>
+
+import unittest
+import vcr
+from pokemontcgsdk import Card
+
+# Python 3.6 Workaround until https://github.com/kevin1024/vcrpy/issues/293 is fixed.
+vcr_connection_request = vcr.stubs.VCRConnection.request 
+vcr.stubs.VCRConnection.request = lambda *args, **kwargs: vcr_connection_request(*args)
+
+class TestCard(unittest.TestCase):
+    def test_find_returns_card(self):
+        with vcr.use_cassette('fixtures/gardevoir.yaml'):
+            card = Card.find('xy7-54')
+
+            self.assertEqual('Gardevoir', card.name)
+            self.assertEqual('xy7-54', card.id)
+            self.assertEqual(282, card.national_pokedex_number)
+            self.assertEqual('https://images.pokemontcg.io/xy7/54.png', card.image_url)
+            self.assertEqual('Stage 2', card.subtype)
+            self.assertEqual('Pokémon', card.supertype)
+            self.assertEqual('Bright Heal', card.ability['name'])
+            self.assertEqual('Once during your turn (before your attack), you may heal 20 damage from each of your Pokémon.',card.ability['text'])
+            self.assertEqual('130', card.hp)
+            self.assertEqual(['Colorless', 'Colorless'], card.retreat_cost)
+            self.assertEqual(2, card.converted_retreat_cost)
+            self.assertEqual('54', card.number)
+            self.assertEqual('TOKIYA', card.artist)
+            self.assertEqual('Rare Holo', card.rarity)
+            self.assertEqual('XY', card.series)
+            self.assertEqual('Ancient Origins', card.set)
+            self.assertEqual('xy7', card.set_code)
+            self.assertEqual(['Fairy'], card.types)
+            self.assertTrue(len(card.attacks) == 1)
+            self.assertTrue(len(card.weaknesses) == 1)
+            self.assertTrue(len(card.resistances) == 1)
+
+    def test_all_with_params_return_cards(self):
+        with vcr.use_cassette('fixtures/mega_pokemon.yaml'):
+            cards = Card.where(supertype='pokemon', subtype='mega')
+            self.assertTrue(len(cards) >= 70)
+
+    def test_all_with_page_returns_cards(self):
+        with vcr.use_cassette('fixtures/all_first_page.yaml'):
+            cards = Card.where(page=1)
+            self.assertEqual(100, len(cards))
+
+    def test_all_with_page_and_page_size_returns_card(self):
+        with vcr.use_cassette('fixtures/all_first_page_one_card.yaml'):
+            cards = Card.where(page=1, pageSize=1)
+            self.assertEqual(1, len(cards))
+
